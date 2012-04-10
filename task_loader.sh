bda37185e64b5eb58955fd412a1b1409151e40c9
Eldar Zakirov <unixod@gmail.com>
2012-04-10 23:13:50 +0500

    Refactoring

--- task_loader.sh
+++ task_loader.sh
@@ -2,35 +2,37 @@
 
 function run_task(){
 	local task_id=$1
+	local complete_tasks_dir=$2
+
 	touch $task_id.$$
 	source $task_id
 	rm $task_id.$$
+	mv $task_id $complete_tasks_dir
 }
 
 function start_tasks(){
+	local complete_tasks_dir=$1
+
 	# lockfile name format: task_id.pid
-	lock_files=$(ls -1 | grep -i '[0-9a-f]\{32\}\.[0-9]*$')
-	lock_files_cnt=$(ls -1 | grep -ci '[0-9a-f]\{32\}\.[0-9]*$')
+	local lock_file=$(ls -1rt | grep -iE '[0-9a-f]{32}\.[0-9]+$' | line)
+	local task_id
 
-	#if exist only one lockfile
-	case $lock_files_cnt in
-		1)	# task is already running, or task previously stoped
-			local task_id=$(echo $lock_files | cut -f1 -d.)
-			local pid=$(echo $lock_files | cut -f2 -d.)
+	if [[ -n $lock_file ]]; then
+		# task is already running, or previously stoped
+		task_id=$(echo $lock_file | cut -f1 -d.)
+		local pid=$(echo $lock_file | cut -f2 -d.)
 
-			# if task is not running (stoped previously)
-			if [[ -z $(ps -e | grep "$pid.*$task_id") ]]; then
-				rm $lock_files
-				run_task $task_id
-			fi
-			;;
-		0)
-			local task_id=$(ls -1rt | line)			#oldest task
-			[ -n task_id ] && run_task $task_id
-			;;
-		*)
-			echo oth
-			;;
-	esac
+		# if task is not running (stoped previously)
+		if [[ -z $(ps -e | grep "$pid.*$task_id") ]]; then
+			#remove old lockfiles
+			find . -regex '^.*/[0-9a-f]{32}\.[0-9]+$' -exec rm {} \;
+		fi	
+	fi
+
+	[[ ! -f $task_id ]] &&
+		task_id=$(ls -1rt --file-type | grep -iE '^[0-9a-f]{32}$' | line)			# oldest task
+
+	[[ -f $task_id ]] &&
+		run_task $task_id $complete_tasks_dir
 }
 
